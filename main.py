6c6a8684d86ccdfe14428766e9ee10e7c7151438
Ricardo Valdarrago <56936568+Ricardo-scb@users.noreply.github.com>
2021-07-06 11:35:00 -0800

    Add files via upload
     The script which runs the simulation of IRE in each position

---
+++ main.py
@@ -0,0 +1,46 @@
+import RNA
+
+
+# simple script to test the free energy of each sequence
+
+#Function to calculate both SEC and MFE
+def Calculate_SEC_MFE(sequences):
+    # RNA Sequence dictory with sequencence and free energy
+    temp_dict = sequences
+
+    # compute minimum free energy (MFE) and corresponding structure for each of the sequences stored in the dictionary
+    for sequence in temp_dict:
+        mfe = RNA.fold(sequence)
+        temp_dict[sequence] = mfe
+
+    # loop through the dictionary and print sequence with the secondary free structure and free energy
+    for sequence in temp_dict:
+        print("Sequence: ", sequence + ", " + "Secondary structure and Free Energy: " + str(temp_dict[sequence]), "\n")
+
+
+# IRE that needs to be inserted
+IRE = "GCUCGUUCCCCACCCCGGCCGGCCGCCC"
+
+# 5 Prime reagion of the Sequence
+FPRIME = "ACATTTGCTTCTGACACAACTGTGTTCACTAGCAACCTCAAACAGACACC"
+
+# dictionary which contains each of the sequences
+mydict = {}
+
+
+# Function that generates the sequences
+def Generate_Sequences(Dict):
+    # loops through the dictionary and inserts the IRE into each position of the 5PRIME region
+    for i in range(len(FPRIME)):
+        test = FPRIME[:i] + IRE + FPRIME[i:]
+        Dict[test] = 0
+
+    #Beacuse of looping issues this used these lines of code to put IRE in the end of the sequence.
+    #!!See if we can just have this happen in the for loop!!
+    Final_Pos = FPRIME + IRE
+    Dict[Final_Pos] = 0
+
+
+#Runs the functions
+Generate_Sequences(mydict)
+Calculate_SEC_MFE(mydict)

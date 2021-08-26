88b880d1233b87abf39f647e77f89b00d913cd91
cgon0198 <86135400+cgon0198@users.noreply.github.com>
2021-08-25 18:03:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,22 @@ def Calculate_SEC_MFE(sequences):
         mfe = RNA.fold(sequence)
         temp_dict[sequence] = mfe
 
+    # Used to index the position of the IRE in the 5'UTR
+    # j = 0
     # loop through the dictionary and print sequence with the secondary free structure and free energy
-    for sequence in temp_dict:
-        print("Sequence: ", sequence + ", " + "Secondary structure and Free Energy: " + str(temp_dict[sequence]), "\n")
+    # for sequence in temp_dict:
+        # print(">" + seq_name + "_" + str(j) + "_" + IRE_name + "\n" + sequence + "\n")
+        # j += 1
 
+
+# Name of Sequence, When changing name make sure there is no spaces
+seq_name = "sequence1"
+
+# Name of IRE, When changing name make sure there is no spaces
+IRE_name = "IRBP1"
 
 # IRE that needs to be inserted
-IRE = "GCUCGUUCCCCACCCCGGCCGGCCGCCC"
+IRE = "AGCGACGACGGTCTACTACTATCTATCATCTAT"
 
 # 5 Prime reagion of the Sequence
 FPRIME = "ACATTTGCTTCTGACACAACTGTGTTCACTAGCAACCTCAAACAGACACC"
@@ -31,23 +40,20 @@ mydict = {}
 # Function that generates the sequences
 def Generate_Sequences(Dict):
     # loops through the dictionary and inserts the IRE into each position of the 5PRIME region
-    for i in range(len(FPRIME)):
+    for i in range(len(FPRIME)+1):
         test = FPRIME[:i] + IRE + FPRIME[i:]
         Dict[test] = 0
 
-    #Beacuse of looping issues this used these lines of code to put IRE in the end of the sequence.
-    #!!See if we can just have this happen in the for loop!!
-    Final_Pos = FPRIME + IRE
-    Dict[Final_Pos] = 0
 
-
-#Runs the functions
+# Runs the functions
 Generate_Sequences(mydict)
 Calculate_SEC_MFE(mydict)
 
 
-# Creates file "results.txt" and writes each key(sequence) and values(2nd struct and mfe) to the file from mydict
-file = open('results.txt', 'w')
+# Creates a .fasta file "results.fasta" and writes each key(sequence) to the file from mydict
+file = open('results.fasta', 'w')
+index = 0
 for results in mydict:
-    file. write('Sequence: ' + results + " " + "Secondary Structure and MFE: " + str(mydict.get(results)) + "\n")
+    file. write(">" + seq_name + "_" + str(index) + "_" + IRE_name + "\n" + results + "\n")
+    index += 1
 file.close()
